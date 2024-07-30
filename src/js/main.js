7f35f5220343fa1c5580ee0b3c2a2eec79926629
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-07-30 12:11:11 +0700

    Next Commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('JavaScript loaded successfully!');
+    // Tambahkan kode JavaScript Anda di sini
+    new kursor({
+        type: 1,
+        color: 'rgb(236, 106, 52)'
+    })
+  });
